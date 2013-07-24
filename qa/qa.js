b2290e0df9c0c5e97ce73e375ab909268240f19a
Hack Reactor Students <github@hackreactor.com>
2013-07-24 11:12:33 -0700

    Add collections and add user button

--- qa/qa.js
+++ qa/qa.js
@@ -1,3 +1,7 @@
+Users = new Meteor.Collection("users");
+Questions = new Meteor.Collection("questions");
+Answers = new Meteor.Collection("answers");
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
     return "Welcome to qa.";
@@ -8,6 +12,14 @@ if (Meteor.isClient) {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
+    }
+  });
+
+  Template.user.events({
+    'click .add_user' : function (){
+      var new_user_name = $('.new_user_name').val();
+      Users.insert({name:new_user_name});
+      console.log(Users.find().fetch());
     }
   });
 }
